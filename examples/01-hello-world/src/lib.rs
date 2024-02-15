22e907110b0bc5156669d3c0c268d774432f8380
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-02-14 22:26:49 -0800

    Try calling a callback after a promise is resolved

--- examples/01-hello-world/src/lib.rs
+++ examples/01-hello-world/src/lib.rs
@@ -1,7 +1,10 @@
-use js_sys::Reflect;
+use js_sys::{Promise, Reflect};
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
-use wasm_react::{export_components, h, Component, VNode};
+use wasm_bindgen_futures::{spawn_local, JsFuture};
+use wasm_react::{
+  export_components, h, hooks::use_state, Callback, Component, VNode,
+};
 
 pub struct App {
   name: Option<Rc<str>>,
@@ -21,14 +24,47 @@ impl TryFrom<JsValue> for App {
 
 impl Component for App {
   fn render(&self) -> VNode {
-    h!(h1).build(
-      //
-      if let Some(name) = self.name.as_ref() {
-        format!("Hello {name}!")
-      } else {
-        "Hello World!".to_string()
-      },
+    let callback_called = use_state(|| false);
+    let callback_called_value = callback_called.value();
+
+    (
+      DelayedButton {
+        callback: {
+          let mut callback_called = callback_called.clone();
+          move || callback_called.set(|_| true)
+        },
+      }
+      .build(),
+      VNode::from(match *callback_called_value {
+        true => "Callback called",
+        false => "Callback not called",
+      }),
     )
+      .into()
+  }
+}
+
+struct DelayedButton<T: FnMut() + Clone> {
+  callback: T,
+}
+
+impl<T: FnMut() + Clone + 'static> Component for DelayedButton<T> {
+  fn render(&self) -> VNode {
+    let mut callback = self.callback.clone();
+    h!(button)
+      .on_click(&Callback::new(move |_| {
+        // This works
+        callback();
+
+        // This doesn't work
+        spawn_local(async move {
+          JsFuture::from(Promise::resolve(&JsValue::UNDEFINED))
+            .await
+            .unwrap();
+          callback();
+        });
+      }))
+      .build("Click me")
   }
 }
 
